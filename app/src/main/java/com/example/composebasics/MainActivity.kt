08e8b997955579d6a95db2a12e8807a29231beeb
Augustinekan <augustinekanyuira0@gmail.com>
2023-06-13 22:50:58 +0300

    replace column with lazy list

--- app/src/main/java/com/example/composebasics/MainActivity.kt
+++ app/src/main/java/com/example/composebasics/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -47,12 +49,11 @@ private fun MyApp(modifier: Modifier = Modifier){
     }
 }
 @Composable
-fun GreetingsComposable(modifier: Modifier = Modifier, names: List<String> = listOf("World", "Compose")){
-    Column(
-        modifier = modifier.padding(vertical = 4.dp)
-    ) {
-        for(name in names){
-            Greeting(name)
+fun GreetingsComposable(modifier: Modifier = Modifier, names: List<String> = List(1000) { "$it" }
+){
+    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
+            Greeting(name = name)
         }
     }
 }
