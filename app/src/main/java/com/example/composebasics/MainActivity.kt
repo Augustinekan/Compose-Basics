0f9587f2a823b5ee89ec31c66d8f503cc1d398d3
Augustinekan <augustinekanyuira0@gmail.com>
2023-06-13 22:43:06 +0300

    hoist state, add onboarding state

--- app/src/main/java/com/example/composebasics/MainActivity.kt
+++ app/src/main/java/com/example/composebasics/MainActivity.kt
@@ -5,9 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -24,23 +23,39 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Preview
 @Composable
-private fun MyApp(modifier: Modifier = Modifier, names: List<String> = listOf("World", "Compose")
-) {
+fun MyAppPreview() {
+    ComposeBasicsTheme {
+        MyApp(Modifier.fillMaxSize())
+    }
+}
+@Composable
+private fun MyApp(modifier: Modifier = Modifier){
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+
     Surface(
         modifier = modifier,
         color = MaterialTheme.colors.background
     ) {
-        Column(
-            modifier = modifier.padding(vertical = 4.dp)
-        ) {
-            for(name in names){
-                Greeting(name)
-            }
+        if (shouldShowOnboarding) {
+            OnBoardingScreen(onContinueClicked = {shouldShowOnboarding = false})
+        } else {
+            GreetingsComposable()
+        }
+
+    }
+}
+@Composable
+fun GreetingsComposable(modifier: Modifier = Modifier, names: List<String> = listOf("World", "Compose")){
+    Column(
+        modifier = modifier.padding(vertical = 4.dp)
+    ) {
+        for(name in names){
+            Greeting(name)
         }
     }
 }
-
 @Composable
 fun Greeting(name: String) {
     val expanded = remember { mutableStateOf(false) }
@@ -53,7 +68,9 @@ fun Greeting(name: String) {
             modifier = Modifier.padding(24.dp)
         ) {
             Column(
-                modifier = Modifier.weight(1f).padding(bottom = extraPadding)
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
 
                 ) {
                 Text(text = "Hello,")
@@ -74,5 +91,35 @@ fun Greeting(name: String) {
 private fun DefaultPreview() {
     ComposeBasicsTheme {
         MyApp()
+    }
+}
+
+
+
+@Composable
+fun OnBoardingScreen(modifier: Modifier = Modifier,
+                     onContinueClicked: () -> Unit,
+                     ) {
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Welcome to the Basics Codelab!")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = onContinueClicked
+        ) {
+            Text("Continue")
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnBoardingPreview() {
+    ComposeBasicsTheme {
+        OnBoardingScreen(onContinueClicked = {})
     }
 }
